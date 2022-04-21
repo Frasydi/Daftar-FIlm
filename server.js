f5b1d64c650fff8e4daa1746c002a8f062452a2e
Frasydi <Frasydi137@Gmail.com>
2022-04-21 19:48:59 +0800

    TuBes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const serveStatic = require("serve-static")
-const path = require('path');
+import express from 'express'
+import serveStatic from "serve-static"
+import path from 'path';
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 80;
