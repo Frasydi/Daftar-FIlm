ea24072d5d57aa327547f9216ee39a34c7a4076a
Frasydi <Frasydi137@Gmail.com>
2022-04-22 13:38:12 +0800

    TuBes

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
+var history = require('connect-history-api-fallback');
 app = express();
 
 app.use(serveStatic(path.join(__dirname, 'dist')));
-
+app.use(history())
 app.get("/add", (req,res) => {
     res.redirect("/")
 })
